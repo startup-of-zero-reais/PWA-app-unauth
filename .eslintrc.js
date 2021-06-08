b8094f2e34f99b892a1a321e329f68f184dff064
Vicente Junior <vicente19981@live.com>
2021-06-07 22:53:10 -0300

    chore: adicionado novas configuracoes do plugin preettier no eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/strict-boolean-expression': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/comma-spacing': 'off',
-    '@typescript-eslint/return-await': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    'no-redeclare': 'off',
-    'import/export': 'off',
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-  },
+    extends: ['plugin:react/recommended', 'airbnb'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/strict-boolean-expression': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/comma-spacing': 'off',
+        '@typescript-eslint/return-await': 'off',
+        '@typescript-eslint/no-namespace': 'off',
+        'no-redeclare': 'off',
+        'import/export': 'off',
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [
+            1,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
+        'react/jsx-props-no-spreading': 'off',
+        indent: ['error', 4],
+    },
 };
